d862163a8b0f4d6bd21171de89a920ea75b9ca0e
marwareda92 <200394610@student.georgianc.on.ca>
2021-03-28 02:25:03 -0400

    Created the Course.java class

--- src/models/Course.java
+++ src/models/Course.java
@@ -8,27 +8,54 @@ public class Course {
     private ArrayList<Student> students;
 
     public Course(String courseCode, String crn, String courseName, Professor prof) {
-        setCourse(courseCode);
+        setCourseCode(courseCode);
         setCrn(crn);
         setCourseName(courseName);
         setProf(prof);
         students = new ArrayList<>();
     }
 
+    /**
+     * This method will add a Student to the course list called students if there is space in the class
+     * class sizes are capped at 40
+     */
+    public void addStudent(Student newStudent){
+        if (students.size()<40)
+            students.add(newStudent);
+        else
+            throw new IllegalArgumentException("Course is full");
+    }
+
     public String getCourseCode() {
         return courseCode;
     }
 
+    /**
+     * This method will validate that the course code matches the correct pattern of COMP XXXX and sets the instance variables
+     * @param courseCode
+     */
     public void setCourseCode(String courseCode) {
-        this.courseCode = courseCode;
+        //if wanted regex for X2XXX5 it would be ("[0-9]2[0-9]{3}5")
+        if (courseCode.matches("COMP [0-9]{4}"))
+            this.courseCode = courseCode;
+        else
+            throw new IllegalArgumentException("Course code must match the pattern COMP XXXX where x is a number");
     }
 
     public String getCrn() {
         return crn;
     }
 
+    public int getNumOfStudents(){
+        return students.size();
+    }
+
     public void setCrn(String crn) {
-        this.crn = crn;
+        crn = crn.trim();
+        if (crn.matches("2[0-9]{4}"))
+            this.crn = crn;
+        else
+            throw new IllegalArgumentException("CRN must match the pattern 2XXXX where x is a number");
     }
 
     public String getCourseName() {
@@ -36,7 +63,11 @@ public class Course {
     }
 
     public void setCourseName(String courseName) {
-        this.courseName = courseName;
+        courseName = courseName.trim();
+        if (courseName.length()>=2 && courseName.length()<=50)
+            this.courseName = courseName;
+        else
+            throw new IllegalArgumentException("Name must be 2-50 characters long");
     }
 
     public Professor getProf() {
